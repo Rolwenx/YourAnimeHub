f1203fa9af67917bffb0221492e0341b91e00571
Rolwen <n.rnolwen@gmail.com>
2023-11-07 00:14:25 +0100

    Created the sitemap of the website

---
+++ webapp/sitemap.ml
@@ -0,0 +1,14 @@
+This XML Sitemap Index file contains 13 sitemaps.
+
+http://localhost:9000
+http://localhost:9000/browse/anime
+http://localhost:9000/browse/manga
+http://localhost:9000/browse/characters
+http://localhost:9000/browse/quotes
+http://localhost:9000/browse/reviews
+http://localhost:9000/login
+http://localhost:9000/signup
+http://localhost:9000/terms
+http://localhost:9000/profile
+http://localhost:9000/profile/watchlist
+http://localhost:9000/profile/settings
